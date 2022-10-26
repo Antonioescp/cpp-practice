9c2ef108ad202d35798fc07d7dfcaf8353879b9d
Antonioescp <packageinstaller2@gmail.com>
2022-10-26 07:54:11 -0600

    MM - Free Store Object Array

---
+++ memory-management/Basic.h
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+class Basic
+{
+
+public:
+	Basic()
+	{
+		cout << "Basic constructed" << endl;
+	}
+
+	~Basic()
+	{
+		cout << "Basic destroyed" << endl;
+	}
+};
--- memory-management/free-store-array.cpp
+++ memory-management/free-store-array.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "Basic.h"
 
 using std::cout;
 using std::endl;
@@ -15,6 +16,14 @@ int main()
 	int size{100};
 	int* arr = new(std::nothrow) int[size]{};
 	delete[] arr;
+	
+	// An array of object using new will
+	// automatically call the zero-initialization
+	// or =default constructor while a primitive
+	// type array would leave the values
+	// uninitialized	
+	Basic* objArr{ new(std::nothrow) Basic[10] };
+	delete[] objArr;
 
 	return 0;
 }
