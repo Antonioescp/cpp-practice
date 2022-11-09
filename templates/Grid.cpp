98e6bf10e1d0b8815e2c6d311392eff1783bcc87
Antonioescp <packageinstaller2@gmail.com>
2022-11-09 00:33:27 -0600

    Moved template specialization implementation to implementation file

--- templates/Grid.cpp
+++ templates/Grid.cpp
@@ -79,5 +79,76 @@ void Grid<Rows, Columns, T>::Swap(Grid& other) noexcept
     std::swap(m_elements, other.m_elements);
 }
 
+template <size_t Rows, size_t Columns>
+Grid<Rows, Columns, const char*>::Grid()
+{
+    m_elements.resize(Rows);
+    for ( auto& column : m_elements )
+    {
+        column.resize(Columns);
+    }
+}
+
+template <size_t Rows, size_t Columns>
+void Grid<Rows, Columns, const char*>::VerifyRange(size_t row, size_t column) const
+{
+    if ( row >= Rows )
+    {
+        throw std::out_of_range{ "Row " + std::to_string(row) + " is out of range." };
+    }
+    else if ( column >= Columns )
+    {
+        throw std::out_of_range{ "Column " + std::to_string(column) + " is out of range." };
+    }
+}
+
+template <size_t Rows, size_t Columns>
+std::optional<std::string>& Grid<Rows, Columns, const char*>::At(size_t row, size_t column)
+{
+    return const_cast<std::optional<std::string>&>( std::as_const(*this).At(row, column) );
+}
+
+template <size_t Rows, size_t Columns>
+const std::optional<std::string>& Grid<Rows, Columns, const char*>::At(size_t row, size_t column) const
+{
+    VerifyRange(row, column);
+    return m_elements[row][column];
+}
+
+template <size_t Rows, size_t Columns>
+template <size_t ERows, size_t EColumns>
+Grid<Rows, Columns, const char*>::Grid(const Grid<ERows, EColumns, const char*>& src)
+{
+    for ( size_t i{}; i < Rows; i++ )
+    {
+        for ( size_t j{}; j < Columns; j++ )
+        {
+            if ( i < ERows && j < EColumns )
+            {
+                m_elements[i][j] = src.At(i, j);
+            }
+            else
+            {
+                m_elements[i][j].reset();
+            }
+        }
+    }
+}
+
+template <size_t Rows, size_t Columns>
+template <size_t ERows, size_t EColumns>
+Grid<Rows, Columns, const char*>& Grid<Rows, Columns, const char*>::operator=(const Grid<ERows, EColumns, const char*>& rhs)
+{
+    Grid<Rows, Columns, const char*> tmp{ rhs };
+    Swap(tmp);
+    return *this;
+}
+
+template <size_t Rows, size_t Columns>
+void Grid<Rows, Columns, const char*>::Swap(Grid& other) noexcept
+{
+    std::swap(m_elements, other.m_elements);
+}
+
 // testing template
 template class Grid<8, 8, std::string>;
